dddedb59b9ec39448eb8890a4d749ca6624a9272
Akshithpottigari <92357179+Akshithpottigari@users.noreply.github.com>
2021-11-19 15:16:36 +0530

    updating calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,39 @@
-const FirstInput  = document.getElementById('FirstInput');
-const SecondInput  = document.getElementById('SecondInput');
-console.log(FirstInput);
+
+var FirstInput='';
+var SecondInput='';
+var result=document.getElementById('result')
+var FirstInput=document.getElementById('FirstInput');
+var SecondInput=document.getElementById('SecondInput');
+var val1=0
+var val2=0;
+
+function calculator(){
+     val1 =  Number(FirstInput.value);
+     console.log(FirstInput)
+     val2 =  Number(SecondInput.value);
+}
+
+function add(){
+    calculator();
+    result.value = (val1 +val2);
+}
+
+function sub(){
+    calculator();
+    result.value = (val1 -val2);
+}
+
+function mul(){
+    calculator();
+    result.value = (val1 *val2);
+} 
+
+function div(){
+    calculator();
+    result.value = (val1 /val2);
+}
+
+function rem(){
+    calculator();
+    result.value = (val1 %val2);
+}
